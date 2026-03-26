40691a13262beda6eb8cffcef7ed4f1102e611de
sritharssamiduraik <sritharsamiduraik@gmail.com>
2026-03-26 07:36:56 +0530

    Update AuthController.java

--- src/main/java/com/klu/controller/AuthController.java
+++ src/main/java/com/klu/controller/AuthController.java
@@ -11,7 +11,7 @@ import com.klu.service.UserService;
 
 @RestController
 @RequestMapping("/api/auth")
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 @CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
